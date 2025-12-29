8b4dc00d4f5b0daaa2635fc82ee44bcb636e8669
Shadow <the.shadow.skript@gmail.com>
2025-12-28 20:09:09 -0500

    005: updated main.py with sys argv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import calculate_number_of_words, calculate_number_of_characters, sort_number_of_characters_into_list
 
 def get_book_text(path_to_file):
@@ -6,14 +7,18 @@ def get_book_text(path_to_file):
   return file_contents
 
 def main():
-  path = "books/frankenstein.txt"
+  if len(sys.argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  path = sys.argv[1]
   book = get_book_text(path)
   word_count = calculate_number_of_words(book)
   character_count = calculate_number_of_characters(book)
   sorted_by_character_count = sort_number_of_characters_into_list(character_count)
   
   print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
+  print(f"Analyzing book found at {path}...")
   print("----------- Word Count ----------")
   print(f"Found {word_count} total words")
   for items in sorted_by_character_count:
